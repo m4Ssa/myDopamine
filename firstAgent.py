f315a7a2ee8feb19be156c1c4a932df4f0712299
alex <alexander.kedzia@lachmann-rink.de>
2018-09-10 09:29:04 +0200

    added firstAgent.py

---
+++ firstAgent.py
@@ -0,0 +1,67 @@
+import numpy as np
+import os
+from dopamine.agents.dqn import dqn_agent
+from dopamine.atari import run_experiment
+from dopamine.colab import utils as colab_utils
+from absl import flags
+
+BASE_PATH = '/tmp/colab_dope_run'  # @param
+GAME = 'Asterix'  # @param
+
+# @title Load baseline data
+!gsutil -q -m cp -R gs://download-dopamine-rl/preprocessed-benchmarks/* /content/
+experimental_data = colab_utils.load_baselines('/content')
+
+# @title Create an agent based on DQN, but choosing actions randomly.
+
+LOG_PATH = os.path.join(BASE_PATH, 'random_dqn', GAME)
+
+class MyRandomDQNAgent(dqn_agent.DQNAgent):
+  def __init__(self, sess, num_actions):
+    """This maintains all the DQN default argument values."""
+    super(MyRandomDQNAgent, self).__init__(sess, num_actions)
+    
+  def step(self, reward, observation):
+    """Calls the step function of the parent class, but returns a random action.
+    """
+    _ = super(MyRandomDQNAgent, self).step(reward, observation)
+    return np.random.randint(self.num_actions)
+
+def create_random_dqn_agent(sess, environment):
+  """The Runner class will expect a function of this type to create an agent."""
+  return MyRandomDQNAgent(sess, num_actions=environment.action_space.n)
+
+# Create the runner class with this agent. We use very small numbers of steps
+# to terminate quickly, as this is mostly meant for demonstrating how one can
+# use the framework. We also explicitly terminate after 110 iterations (instead
+# of the standard 200) to demonstrate the plotting of partial runs.
+random_dqn_runner = run_experiment.Runner(LOG_PATH,
+                                          create_random_dqn_agent,
+                                          game_name=GAME,
+                                          num_iterations=200,
+                                          training_steps=10,
+                                          evaluation_steps=10,
+                                          max_steps_per_episode=100)
+
+# @title Train MyRandomDQNAgent.
+print('Will train agent, please be patient, may be a while...')
+random_dqn_runner.run_experiment()
+print('Done training!')
+
+# @title Load the training logs.
+random_dqn_data = colab_utils.read_experiment(LOG_PATH, verbose=True)
+random_dqn_data['agent'] = 'MyRandomDQN'
+random_dqn_data['run_number'] = 1
+experimental_data[GAME] = experimental_data[GAME].merge(random_dqn_data,
+                                                        how='outer')
+
+# @title Plot training results.
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(figsize=(16,8))
+sns.tsplot(data=experimental_data[GAME], time='iteration', unit='run_number',
+           condition='agent', value='train_episode_returns', ax=ax)
+plt.title(GAME)
+plt.show()
